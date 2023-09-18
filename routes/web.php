d92be0aa542845136506cb5d2944dee2695d3e49
JetsadaSomporn <jetsadasomporn2003@gmail.com>
2023-09-18 19:27:06 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('poser/home', [HomeController::class, 'poserHome'])->name('poser.home')->middleware('is_poser');
-Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_poser');
-Route::resource('jobinfo', postcRUDController::class);
-Route::get('createjob', [postcRUDController::class, 'create']);
+    Route::get('poser/home', [HomeController::class, 'poserHome'])->name('poser.home')->middleware('is_poser');
+    Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_poser');
+    Route::resource('jobinfo', postcRUDController::class);
+    Route::get('createjob', [postcRUDController::class, 'create'])->name('create');
+    Route::post('createjob2', [postcRUDController::class, 'create2'])->name('create2');
+    Route::post('store', [postcRUDController::class, 'store'])->name('store');
+    Route::get('enroll', [enrollController::class, 'enroll']);
+});
