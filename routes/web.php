1a98755386ef18d2436f26e019291dc64d872329
JetsadaSomporn <jetsadasomporn2003@gmail.com>
2023-09-20 15:56:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{
+    ProfileController,
+    postcRUDController,
+    enrollController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
