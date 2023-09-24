f1cfa56109b612a534052f6ee0845b2498afd48f
JetsadaSomporn <jetsadasomporn2003@gmail.com>
2023-09-24 14:35:02 +0700

    Handle PDF over 10MB

--- resources/views/enroll/enrollwork.blade.php
+++ resources/views/enroll/enrollwork.blade.php
@@ -1,4 +1,22 @@
+<script>
+    document.getElementById('resume').addEventListener('change', function(event) {
+        const fileInput = event.target;
+        const maxSize = 10 * 1024 * 1024;
+
+        if (fileInput.files.length > 0) {
+            const fileSize = fileInput.files[0].size;
+
+            if (fileSize > maxSize) {
+                document.getElementById('file-size-error').textContent = 'File size exceeds 10MB limit.';
+                fileInput.value = '';
+            } else {
+                document.getElementById('file-size-error').textContent = '';
+            }
+        }
+    });
+</script>
 <x-app-layout>
+
     <body>
         <div class="distance_top"></div>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
@@ -33,7 +51,8 @@
                 <input type="tel" name="phone" id="phone" class="input-field" value="{{ auth()->user()->phonenumber }}" required>
 
                 <label for="resume">Upload Resume (PDF, max 10MB):</label>
-                <input type="file" name="resume" id="resume" accept=".pdf" class="form-file-input" style="background-color: white; color: #4869D9; width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 4px; font-size: 16px; cursor: pointer; margin-bottom: 10px;" required>
+<input type="file" name="resume" id="resume" accept=".pdf" class="form-file-input" style="background-color: white; color: #4869D9; width: 100%; padding: 10px; border: 1px solid #ccc; border-radius: 4px; font-size: 16px; cursor: pointer; margin-bottom: 10px;" required>
+
 
                 <input type="hidden" name="resume_file_name" value="{{ session('resumeFileName') }}">
                 <button type="submit" class="submit-button">Next</button>
